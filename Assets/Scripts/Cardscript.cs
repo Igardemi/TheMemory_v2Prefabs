17f12fed0a981d48011a541810ce63f160ef9594
Igardemi <74794797+Igardemi@users.noreply.github.com>
2021-04-26 14:55:56 +0200

    subimos la carpeta Assets
    Scripts e imagenes

---
+++ Assets/Scripts/Cardscript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Cardscript : MonoBehaviour
+{
+
+    public Sprite Front;
+    public Sprite Back;
+    public int Value;
+    private SpriteRenderer carta;
+    private bool FaceUp = false;
+    public int Index;
+
+    public GameObject gm;
+
+
+    public void Awake()
+    {
+        carta = GetComponent<SpriteRenderer>();
+        gm = GameObject.FindGameObjectWithTag("Manager");
+        //carta.sprite = Back;
+
+    }
+    public void Toggle()
+    {
+        //if (FaceUp == false)
+        //{
+        //    carta.sprite = Front;
+        //    FaceUp = true;
+        //}
+        //else if (FaceUp == true)
+        //{
+        //    carta.sprite = Back;
+        //    FaceUp = false;
+        //}
+
+        StartCoroutine("Espera");
+
+    }
+
+    IEnumerator Espera()
+    {
+        if (FaceUp == false)
+        {
+            carta.sprite = Front;
+            FaceUp = true;
+        }
+        else if (FaceUp == true)
+        {
+            yield return new WaitForSeconds(1f);
+            carta.sprite = Back;
+            FaceUp = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Has clickado en una carta.");
+        gm.GetComponent<GameManager>().MoveState(Index);
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    GameObject[] baraja = new GameObject[10];
+
+    public int[] CardsUp = new int[2];
+    public string State = "Inicial";
+
+    public Sprite[] Cartas = new Sprite[5];
+    public GameObject Marcador;
+    public GameObject PrefabCard;
+    int parejas = 0;
+
+    public void Start()
+    {
+
+        Marcador.GetComponent<Text>().text = "Parejas " + parejas;
+
+        for (int i = 0; i < 10; i++)
+        {
+            PrefabCard.name = "PrefabCard" + i;
+            PrefabCard.GetComponent<Cardscript>().Index = i;
+
+            if (i < 5)
+            {
+                PrefabCard.GetComponent<Cardscript>().Value = i;
+                PrefabCard.GetComponent<Cardscript>().Front = Cartas[i];
+                Instantiate(PrefabCard, new Vector3(i - (8 - 3 * i), 2, 0), Quaternion.identity);
+            }
+            else
+            {
+
+                PrefabCard.GetComponent<Cardscript>().Value = i - 5;
+                PrefabCard.GetComponent<Cardscript>().Front = Cartas[i - 5];
+                Instantiate(PrefabCard, new Vector3((i - 5) - (8 - 3 * (i - 5)), -1, 0), Quaternion.identity);
+            }
+
+            
+        }
+
+        baraja = GameObject.FindGameObjectsWithTag("Card");
+
+        CardsUp[0] = -1;
+        CardsUp[1] = -1;
+
+    }
+
+
+    public void MoveState(int index)
+    {
+        GameObject Card1;
+        GameObject Card2;
+
+        if (State == "Inicial")
+        {
+            baraja[index].GetComponent<Cardscript>().Toggle();
+            CardsUp[0] = index;
+            State = "carta_descubierta";
+        }
+        else if (State=="carta_descubierta" && CardsUp[0] != index)
+        {
+            baraja[index].GetComponent<Cardscript>().Toggle();
+
+            CardsUp[1] = index;
+
+            Card1 = baraja[CardsUp[0]];
+            Card2 = baraja[CardsUp[1]];
+
+            
+
+            if ((Card1.GetComponent<Cardscript>().Value) == (Card2.GetComponent<Cardscript>().Value))
+            {
+                Debug.Log("Las cartas son pareja.");
+                parejas++;
+                Marcador.GetComponent<Text>().text = "Parejas " + parejas;
+
+                //Desactivar las cartas
+                baraja[CardsUp[0]].gameObject.SetActive(false);
+                baraja[CardsUp[1]].gameObject.SetActive(false);
+
+                State = "Inicial";
+                
+
+                if (parejas == 5)
+                {
+                    Marcador.GetComponent<Text>().text = "Fin del Juego";
+                }
+                else
+                {
+                    Marcador.GetComponent<Text>().text = "Parejas " + parejas;
+                }
+            }
+            //NO ACIERTO
+            else
+            {
+
+                //baraja[CardsUp[1]].GetComponent<Cardscript>().Toggle();
+                
+                baraja[CardsUp[0]].GetComponent<Cardscript>().Toggle();
+                baraja[CardsUp[1]].GetComponent<Cardscript>().Toggle();
+               
+
+                State = "Inicial";
+                //Dar la vuelta a las cartas.
+
+            }
+        }
+    }
+}
+
+
+
